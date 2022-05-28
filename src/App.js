741f1e443a2eb6c78cb5df4c1ccb47212ea99281
hichem <hichemhamdy@gmail.com>
2022-05-28 14:36:35 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Component from './component';
+function App() {
+
+  return (
+    <div className="App">
+   <Component/>
+    </div>
+  );
+}
+
+export default App;
